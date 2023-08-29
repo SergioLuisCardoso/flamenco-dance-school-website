bf86552581784a00d43888fc31ae788ed4fc96a3
SergioLuisCardoso <sergiolucard@gmail.com>
2023-08-29 17:19:38 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flamenco-dance-school-website
+Development of the Pasión Flamenco Dance School website. The technologies that will be used in the development process include HTML, CSS, Javascript/Jquery and the flexbox methodology, aiming to guarantee the full responsiveness of the interface in different screen resolutions!
---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+/*
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,     
+  });
+
+*/
+
+
+
+
+const images = document.querySelectorAll('.slider img');
+let currentImageIndex = 0;
+
+function showNextImage() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+}
+
+setInterval(showNextImage, 3000); // Troca a imagem a cada 3 segundos
